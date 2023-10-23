f342c97579667c0341dcf803450f197c14875fa9
Katrechko_A_S <crossworld@yandex.ru>
2023-10-23 16:49:54 +0300

    Removed Lion's dependence on Feline

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class Lion {
 
     boolean hasMane;
-
-    public Lion(String sex) throws Exception {
+    Predator predator;
+    public Lion(Predator predator, String sex) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -14,12 +14,11 @@ public class Lion {
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
+        this.predator=predator;
     }
 
-    Feline feline = new Feline();
-
     public int getKittens() {
-        return feline.getKittens();
+        return predator.getKittens();
     }
 
     public boolean doesHaveMane() {
@@ -27,6 +26,6 @@ public class Lion {
     }
 
     public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
+        return predator.eatMeat();
     }
 }
