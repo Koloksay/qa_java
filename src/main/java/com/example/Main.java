3a6d6d7df59b1748839015ccc80b4b59781c37c8
Katrechko A.S <crossworld@yandex.ru>
2023-10-30 12:45:18 +0100

    add jacoco report

--- src/main/java/com/example/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Feline filine = new Feline();
-        Lion lion = new Lion(filine,"Самец");
-        System.out.println(lion.getFood());
-    }
-}
